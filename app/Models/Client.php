b0845eced619db29ff41d064d90ed74ae4142d0c
edison6009 <edison09universidad@gmail.com>
2024-01-26 18:05:02 -0400

    clients

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'last_name',
+        'address',
+        'state_id',
+        'phone',
+        'email',
+        'ci',
+     ] ;
+
+    Public Function scopeFilter ($query, $request) {
+
+    return $query->when($request->name, function ($clients, $name){
+            return $clients->where('name',$name);}
+    )->when($request->last_name, function ($clients, $last_name){
+            return $clients->where('last_name', $last_name);}
+    )->when($request->address, function ($clients, $address){
+            return $clients->where('address', $address);}
+    )->when($request->state_id, function ($clients, $state_id){
+            return $clients->where('state_id', $state_id);}
+    )->when($request->phone, function ($clients, $phone){
+            return $clients->where('phone', $phone);}
+    )->when($request->email, function ($clients, $email){
+            return $clients->where('email', $email);}
+    )->when($request->ci, function ($clients, $ci){
+            return $clients->where('ci', $ci);}
+    );
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/client', [ClientController::class, 'index']);
+Route::post('/client', [ClientController::class, 'store']);
+Route::get('/client/{id}', [ClientController::class, 'show']);
+Route::put('/client/{id}', [ClientController::class, 'update']);
+Route::delete('/client/{id}', [ClientController::class, 'destroy']);
+Route::get('/client/restore/{id}', [ClientController::class, 'restore']);
