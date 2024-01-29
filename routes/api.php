87c5a4c37119d584beef39c09904ab51fc374f80
Susandepablos14 <susanb140903@gmail.com>
2024-01-29 13:01:28 -0400

    actualizacion 11

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ClotheController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,9 +31,38 @@ Route::put('/brand/{id}', [BrandController::class, 'update']);
 Route::delete('/brand/{id}', [BrandController::class, 'destroy']);
 Route::get('/brand/restore/{id}', [BrandController::class, 'restore']);
 
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::put('/product/{id}', [ProductController::class, 'update']);
+Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+Route::get('/product/restore/{id}', [ProductController::class, 'restore']);
+
 Route::get('/client', [ClientController::class, 'index']);
 Route::post('/client', [ClientController::class, 'store']);
 Route::get('/client/{id}', [ClientController::class, 'show']);
 Route::put('/client/{id}', [ClientController::class, 'update']);
 Route::delete('/client/{id}', [ClientController::class, 'destroy']);
 Route::get('/client/restore/{id}', [ClientController::class, 'restore']);
+
+Route::get('/states', [StateController::class, 'index']);
+Route::post('/state', [StateController::class, 'store']);
+Route::get('/state/{id}', [StateController::class, 'show']);
+Route::put('/state/{id}', [StateController::class, 'update']);
+Route::delete('/state/{id}', [StateController::class, 'destroy']);
+Route::get('/state/restore/{id}', [StateController::class, 'restore']);
+
+Route::get('/carts', [CartController::class, 'index']);
+Route::post('/cart', [CartController::class, 'store']);
+Route::get('/cart/{id}', [CartController::class, 'show']);
+Route::put('/cart/{id}', [CartController::class, 'update']);
+Route::delete('/cart/{id}', [CartController::class, 'destroy']);
+Route::get('/cart/restore/{id}', [CartController::class, 'restore']);
+
+Route::get('/clothes', [ClotheController::class, 'index']);
+Route::post('/clothe', [ClotheController::class, 'store']);
+Route::get('/clothe/{id}', [ClotheController::class, 'show']);
+Route::put('/clothe/{id}', [ClotheController::class, 'update']);
+Route::delete('/clothe/{id}', [ClotheController::class, 'destroy']);
+Route::get('/clothe/restore/{id}', [ClotheController::class, 'restore']);
+
