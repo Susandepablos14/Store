a4ec0eb64a82e0373acb9b6df7487a5a534567be
edison6009 <edison09universidad@gmail.com>
2024-02-01 18:33:03 -0400

    posible error

---
+++ app/Http/Controllers/DetailcartController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Detailcart;
+use App\Http\Requests\StoreDetailcartRequest;
+use App\Http\Requests\UpdateDetailcartRequest;
+
+class DetailcartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDetailcartRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Detailcart $detailcart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDetailcartRequest $request, Detailcart $detailcart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Detailcart $detailcart)
+    {
+        //
+    }
+}
