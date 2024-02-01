9ebbc2f8f77959a8d90edbaeec7ed753822e50e8
Susandepablos14 <susanb140903@gmail.com>
2024-02-01 19:04:45 -0400

    actualizacion

--- database/migrations/2024_02_01_215708_create_detailcarts_table.php
+++ database/migrations/2024_02_01_215708_create_detailcarts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('cart_id');
             $table->unsignedBigInteger('product_id');
-            $table->bigInteger('Quantity');
+            $table->bigInteger('quantity');
             $table->softDeletes();
             $table->timestamps();
 
